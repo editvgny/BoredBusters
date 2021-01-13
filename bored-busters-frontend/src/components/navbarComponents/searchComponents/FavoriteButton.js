0829e7a6cb35ab8497b6af0d303b0499d52b1a87
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-13 11:10:42 +0100

    Fix: Now the user can delete card from favorites on favorites page and the page will rerender with the remaining data.

--- bored-busters-frontend/src/components/navbarComponents/searchComponents/FavoriteButton.js
+++ bored-busters-frontend/src/components/navbarComponents/searchComponents/FavoriteButton.js
@@ -1,44 +1,47 @@
 import React, {useState, useEffect} from "react";
-import { FaHeart } from 'react-icons/fa';
+import {FaHeart} from 'react-icons/fa';
 import StyledFavButton from '../../styledComponents/StyledFavButton';
 import axios from "axios";
 
 export default function FavoriteButton(props) {
-  const [activity, setActivity] = useState([]);
+    const [activity, setActivity] = useState([]);
 
-  useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/api/get-activity/${props.activity.activity}`)
-        .then((response) => {
-          setActivity(response.data)
-        })
-  }, [props.activity])
+    useEffect(() => {
+        if (props.activity) {
+            setActivity(props.activity)
+        } else {
+            axios.get(`http://127.0.0.1:8000/api/get-activity/${props.activity.activity}`)
+                .then((response) => {
+                    setActivity(response.data)
+                })
+        }
+    }, [props.activity])
 
-  const updateFavorites = () => {
+    const updateFavorites = () => {
 
-    if (activity.id) {
-      axios.delete(`http://127.0.0.1:8000/api/favorite/${activity.id}`)
-          .then((response) => {
-                setActivity([])
-              }
-          )
-    } else {
-      axios.post('http://127.0.0.1:8000/api/favorite', props.activity)
-          .then((response) => {
-            setActivity(response.data)
-          })
+        if (activity.id) {
+            axios.delete(`http://127.0.0.1:8000/api/favorite/${activity.id}`)
+                .then((response) => {
+                        setActivity([])
+                    axios.get("http://127.0.0.1:8000/api/favorite/1")
+                        .then((response) => {
+                            props.setFavorites(response.data)
+                        })
+                    }
+                )
+        } else {
+            axios.post('http://127.0.0.1:8000/api/favorite', activity)
+                .then((response) => {
+                    setActivity(response.data)
+                })
+        }
     }
 
-    if (props.setCurrentPageOriginals && props.currentPageOriginals > 1 && props.originalCurrentPost.length === 1) {
-      props.setCurrentPageOriginals(props.currentPageOriginals - 1);
-    }
-
-  }
-
-  return (
-    <StyledFavButton style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }} onClick={updateFavorites}>
-      {(activity.id || props.activity.id) ?
-        <div style={{ color: "red" }}><FaHeart style={{ height: "40px", width: "40px" }} /></div>
-        : <div><FaHeart style={{ height: "40px", width: "40px" }} /></div>}
-    </StyledFavButton>
-  )
+    return (
+        <StyledFavButton style={{marginRight: "auto", marginLeft: "auto", minHeight: "50px"}} onClick={updateFavorites}>
+            {(activity.id) ?
+                <div style={{color: "red"}}><FaHeart style={{height: "40px", width: "40px"}}/></div>
+                : <div><FaHeart style={{height: "40px", width: "40px"}}/></div>}
+        </StyledFavButton>
+    )
 }
