887bccf8b65d1ccbf2e299173fd2156012cc02d8
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-13 15:38:45 +0100

    Fix: Search component was refactored and outsourced to FavoriteSearch.js. Ready for implement api call to back end to search by different values.

---
+++ bored-busters-frontend/src/components/navbarComponents/searchComponents/FavoriteSearch.js
@@ -0,0 +1,82 @@
+import React, {useState, useContext} from 'react';
+import StyledSelectContainer from "../../styledComponents/StyledSelectContainer";
+import StyledInputContainer from "../../styledComponents/StyledInputContainer";
+import SliderBar from "../../slidebarComponents/Sliderbar";
+import StyledGetButton from "../../styledComponents/StyledGetButton";
+import StyledFavoriteSearchContainer from "../../styledComponents/StyledFavoriteSearchContainer";
+import {SlideValueContext} from "../../../contextComponents/SlideValueContext";
+
+function FavoriteSearch(props) {
+    const types = ["all", "education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+    const [activityType, setActivityType] = useState("");
+    const [inputParticipants, setParticipants] = useState("");
+    const [priceValues] = useContext(SlideValueContext);
+
+    const clearFields = () => {
+        setParticipants("");
+    };
+
+    const filterActivities = () => {
+        const filterData = {
+            activityMinPrice: priceValues.min,
+            activityMaxPrice: priceValues.max,
+        }
+        if (activityType) {
+            filterData.activityType = activityType;
+        }
+        if (inputParticipants) {
+            filterData.activityParticipants = inputParticipants;
+        }
+        console.log(filterData);
+    }
+
+    return (
+        <StyledFavoriteSearchContainer>
+
+            {/* ************* */}
+            {/* Activity Type */}
+            <StyledSelectContainer>Activity types:
+                <select className="option" onChange={
+                    e => e.target.value==="all" ? setActivityType("") : setActivityType(e.target.value)
+                }>
+                    {/*<option value="">all</option>*/}
+                    {types.map((type) => (
+                        <option key={type} value={type}>{type}</option>
+                    ))}
+                </select>
+            </StyledSelectContainer>
+
+            {/* ********************** */}
+            {/* Number of participants */}
+            <StyledInputContainer>
+                Number of participants:
+                <input
+                    className="input"
+                    type="text"
+                    value={inputParticipants}
+                    placeholder={inputParticipants}
+                    onChange={(e) => setParticipants(e.target.value)}
+                    onFocus={clearFields}
+                />
+            </StyledInputContainer>
+
+            {/* *********** */}
+            {/* Price range */}
+            <SliderBar className="slider"/>
+
+            {/* ************* */}
+            {/* Search button */}
+            <StyledGetButton style={{margin: "5px auto"}} onClick={filterActivities}>
+                Filter my favorites!
+            </StyledGetButton>
+
+            {/* ************* */}
+            {/* No result box */}
+            {props.visibleFavorites.length === 0 ? (
+                <div className="no-result">No results were found</div>) : ("")};
+
+        </StyledFavoriteSearchContainer>
+    );
+}
+
+export default FavoriteSearch;
