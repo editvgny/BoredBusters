207f5c121d042b3647bb52666ca612a2ccd40039
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-01-14 09:59:11 +0100

    ActivityCardDetails.js modification on price. Price is shown by dollar signs not with numbers.

--- bored-busters-frontend/src/components/ActivityCardDetails.js
+++ bored-busters-frontend/src/components/ActivityCardDetails.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { FaDollarSign } from "react-icons/fa";
+
 
 export default function ActivityCardDetails(props) {
     const { activity, type, participants, price, link } = props.activity;
+
+    const createPriceSign = (price) => {
+        if (price <= 0.1) {
+            return <React.Fragment>
+                <FaDollarSign style={{color: 'red'}}/>
+                <FaDollarSign/>
+                <FaDollarSign/>
+                    </React.Fragment>
+        } else if (price > 0.1 && price <= 0.3) {
+            return <React.Fragment>
+                <FaDollarSign style={{color: 'red'}}/>
+                <FaDollarSign style={{color: 'red'}}/>
+                <FaDollarSign/>
+            </React.Fragment>
+        } else {
+            return <React.Fragment>
+                <FaDollarSign style={{color: 'red'}}/>
+                <FaDollarSign style={{color: 'red'}}/>
+                <FaDollarSign style={{color: 'red'}}/>
+            </React.Fragment>
+        }
+    }
 
     return (
         <div className="details">
@@ -12,7 +36,7 @@ export default function ActivityCardDetails(props) {
 
             <div>Number of participants:{participants}</div>
 
-            <div> Price: {price * 10000}</div>
+            <div> Price: {createPriceSign(price)}</div>
 
             {link ? (
                 <div>
