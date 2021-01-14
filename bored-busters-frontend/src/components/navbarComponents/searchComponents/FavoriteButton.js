4156dc1b89cddf4203fa65483c4ccbe37bc79e63
Vivien Takács <takacsvivienbuda@gmail.com>
2021-01-14 14:57:16 +0100

    const token added to FavoriteButton.js and axios was extended with config "header"

--- bored-busters-frontend/src/components/navbarComponents/searchComponents/FavoriteButton.js
+++ bored-busters-frontend/src/components/navbarComponents/searchComponents/FavoriteButton.js
@@ -6,46 +6,74 @@ import Cookies from "js-cookie";
 
 export default function FavoriteButton(props) {
     const [activity, setActivity] = useState([]);
+    let token = document.head.querySelector('meta[name="csrf-token"]');
+    if (!token) {
+        console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    }
 
     useEffect(() => {
         if (props.activity) {
             setActivity(props.activity)
         } else {
-            axios.get(`http://127.0.0.1:8000/api/get-activity/${props.activity.activity}`)
-                .then((response) => {
-                    setActivity(response.data)
+            axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+                axios.get(`http://127.0.0.1:8000/api/get-activity/${props.activity.activity}`, {
+                    headers: {
+                        'X-CSRFTOKEN': token,
+                    }
                 })
+                    .then((response) => {
+                        setActivity(response.data)
+                    })
+            })
         }
     }, [props.activity])
 
     const updateFavorites = () => {
 
         if (activity.id) {
-            axios.delete(`http://127.0.0.1:8000/api/favorite/${activity.id}`)
-                .then((response) => {
+            axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+                axios.delete(`http://127.0.0.1:8000/api/favorite/${activity.id}`, {
+                    headers: {
+                        'X-CSRFTOKEN': token,
+                    }
+                })
+                    .then((response) => {
                         setActivity([])
 
-                    if (props.setActivity) {
-                        props.setActivity([])
-                    }
-                    if (props.setFavorites) {
-                        axios.get("http://127.0.0.1:8000/api/favorite/1")
-                            .then((response) => {props.setFavorites(response.data)})
-                    }
+                        if (props.setActivity) {
+                            props.setActivity([])
+                        }
+                        if (props.setFavorites) {
+                            axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+                                axios.get("http://127.0.0.1:8000/api/favorite/1", {
+                                    headers: {
+                                        'X-CSRFTOKEN': token,
+                                    }
+                                })
+                                    .then((response) => {props.setFavorites(response.data)})
+                            })
+                        }
 
-                    // This section checks if visibleFavorites is empty or not. And if it is it changes
-                    // the actualPageNumber and the visibleFavorites too.
-                    // -1 is because async calls. When visibleFavorites has one element and we get here it will surely
-                    // delete it and it's length will be 0
-                    if (props.visibleFavorites && props.visibleFavorites.length-1 === 0) {
-                        props.refreshVisibleFavorites(props.actualPageNumber-1)
+                        // This section checks if visibleFavorites is empty or not. And if it is it changes
+                        // the actualPageNumber and the visibleFavorites too.
+                        // -1 is because async calls. When visibleFavorites has one element and we get here it will surely
+                        // delete it and it's length will be 0
+                        if (props.visibleFavorites && props.visibleFavorites.length - 1 === 0) {
+                            props.refreshVisibleFavorites(props.actualPageNumber - 1)
+                        }
+                    })
+            })
+        } else {
+            axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+                axios.post('http://127.0.0.1:8000/api/favorite', activity, {
+                    headers: {
+                        'X-CSRFTOKEN': token,
                     }
                 })
-        } else {
-            axios.post('http://127.0.0.1:8000/api/favorite', activity)
-                .then((response) => {
-                    setActivity(response.data)
-                })
+                    .then((response) => {
+                        setActivity(response.data)
+                    })
+            })
         }
     }
 
@@ -55,5 +83,5 @@ export default function FavoriteButton(props) {
                 <div style={{color: "red"}}><FaHeart style={{height: "40px", width: "40px"}}/></div>
                 : <div><FaHeart style={{height: "40px", width: "40px"}}/></div>}
         </StyledFavButton>
-    ): ("")
+    ) : ("")
 }
