b14b5c22f7a3bd6c7cef098ee125141dc7376d39
Vivien Takács <takacsvivienbuda@gmail.com>
2021-02-10 10:41:46 +0100

    created StyledInstruction.js

---
+++ bored-busters-frontend/src/components/styledComponents/StyledInstruction.js
@@ -0,0 +1,12 @@
+import  styled from 'styled-components';
+
+export default styled.p`
+
+      text-align: center;
+      font-family: 'Mountains of Christmas';
+      font-size: 30px;
+      font-weight: 900;
+      letter-spacing: 7px;
+      color: #72CD55;
+
+`;
