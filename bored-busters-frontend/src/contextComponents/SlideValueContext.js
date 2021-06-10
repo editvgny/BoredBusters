bf9248f9196ddde9e7de50ee1b48b9efb1bef089
editvgny <edit.vgny@gmail.com>
2021-06-10 10:44:36 +0200

    Formatted SlideValueContext.js and ThemeContext.js with ctrl+alt+l

--- bored-busters-frontend/src/contextComponents/SlideValueContext.js
+++ bored-busters-frontend/src/contextComponents/SlideValueContext.js
@@ -1,12 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, {createContext, useState} from "react";
 
 export const SlideValueContext = createContext([]);
 
 export function SlideValueContextProvider(props) {
-  const [inputValues, setInputValues] = useState({ min: 0, max: 10000 });
-  return (
-    <SlideValueContext.Provider value={[inputValues, setInputValues]}>
-      {props.children}
-    </SlideValueContext.Provider>
-  );
+    const [inputValues, setInputValues] = useState({min: 0, max: 10000});
+
+    return (
+        <SlideValueContext.Provider value={[inputValues, setInputValues]}>
+            {props.children}
+        </SlideValueContext.Provider>
+    );
 }
