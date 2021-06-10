90829c8cc62a43d36db2e4e9ad60fb8c80cf883d
editvgny <edit.vgny@gmail.com>
2021-06-10 11:24:00 +0200

    Formatted line breaks in Navbar.js

--- bored-busters-frontend/src/components/Navbar.js
+++ bored-busters-frontend/src/components/Navbar.js
@@ -3,14 +3,25 @@ import StyledNavbar from "./styledComponents/StyledNavbar"
 import {Link} from "react-router-dom"
 import Cookies from "js-cookie";
 
-export default function Header() {
+export default function Navbar() {
     return (
         <StyledNavbar>
-            {!Cookies.get('token') ? (<React.Fragment><Link to="/registration">Registration</Link>
-                {''}|{''}
-                <Link to="/login">Login</Link>{''}|{''}
-            </React.Fragment>) : (<React.Fragment><Link to="/logout">Logout</Link>
-                {''}|{''}<Link to="/favorites">Favorites</Link>{''}|{''}</React.Fragment>)}
+            {!Cookies.get('token')
+                ? (
+                    <React.Fragment>
+                        <Link to="/registration">Registration</Link>
+                        {''}|{''}
+                        <Link to="/login">Login</Link>
+                        {''}|{''}
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Link to="/logout">Logout</Link>
+                        {''}|{''}
+                        <Link to="/favorites">Favorites</Link>
+                        {''}|{''}
+                    </React.Fragment>
+                )}
             <Link to="/random">Random</Link>
             {''}|{''}
             <Link to="/type">Type</Link>
@@ -18,8 +29,6 @@ export default function Header() {
             <Link to="/participants">Participants</Link>
             {''}|{''}
             <Link to="/cost">Cost</Link>
-
-
         </StyledNavbar>
     )
 }
