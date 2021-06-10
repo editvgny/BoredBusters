7f99691129979ad04f02f8c99286c011f5232609
editvgny <edit.vgny@gmail.com>
2021-06-10 11:06:24 +0200

    Formatted SearchByParticipants.js

--- bored-busters-frontend/src/components/navbarComponents/searchComponents/SearchByParticipants.js
+++ bored-busters-frontend/src/components/navbarComponents/searchComponents/SearchByParticipants.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import StyledGetButton from "../../styledComponents/StyledGetButton";
 import StyledActivityContainer from "../../styledComponents/StyledActivityContainer";
@@ -8,72 +8,69 @@ import FavoriteButton from "./FavoriteButton"
 
 
 export default function SearchByParticipants() {
-  const [participantsActivity, setParticipantsActivity] = useState([]);
-  const [inputParticipants, setParticipants] = useState([]);
+    const [participantsActivity, setParticipantsActivity] = useState([]);
+    const [inputParticipants, setParticipants] = useState([]);
 
-  const getParticipantsActivity = () => {
-    axios
-      .get(
-        `http://www.boredapi.com/api/activity?participants=${inputParticipants}`
-      )
-      .then((response) => setParticipantsActivity(response.data));
-  };
+    const getParticipantsActivity = () => {
+        axios.get(
+                `http://www.boredapi.com/api/activity?participants=${inputParticipants}`
+            )
+            .then((response) =>
+                setParticipantsActivity(response.data)
+            );
+    };
 
-  const clearFields = () => {
-    setParticipants("");
-  };
+    const clearFields = () => {
+        setParticipants("");
+    };
 
-  return participantsActivity.length !== 0 ? (
-    <StyledActivityContainer>
-      <StyledInputContainer>
-        Number of participants:
-        <input
-          className="input"
-          type="text"
-          value={inputParticipants}
-          placeholder={inputParticipants}
-          onChange={(e) => setParticipants(e.target.value)}
-          onFocus={clearFields}
-        />
-      </StyledInputContainer>
-
-      <StyledGetButton
-        style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }}
-        onClick={getParticipantsActivity}
-      >
-        Give me the activity!
-      </StyledGetButton>
-
-      {participantsActivity.activity ? (
-        <React.Fragment>
-          <FavoriteButton activity={participantsActivity} setActivity={setParticipantsActivity}/>
-          <ActivityCardDetails activity={participantsActivity} />
-        </React.Fragment>
-      ) : (
-          <div style={{ margin: "20px auto" }}>No activity was found!</div>
-        )}
-
-    </StyledActivityContainer>
-  ) : (
-      <StyledActivityContainer>
-        <StyledInputContainer>
-          Number of participants:
-        <input
-            className="input"
-            type="text"
-            value={inputParticipants}
-            placeholder={inputParticipants}
-            onChange={(e) => setParticipants(e.target.value)}
-            onFocus={clearFields}
-          />
-        </StyledInputContainer>
-
-        <StyledGetButton
-          style={{ marginRight: "auto", marginLeft: "auto", minHeight: "50px" }}
-          onClick={getParticipantsActivity}
-        >
-          Give me the activity!
-      </StyledGetButton>
-      </StyledActivityContainer>
+    return participantsActivity.length !== 0 ? (
+        <StyledActivityContainer>
+            <StyledInputContainer>
+                Number of participants:
+                <input
+                    className="input"
+                    type="text"
+                    value={inputParticipants}
+                    placeholder={inputParticipants}
+                    onChange={(e) => setParticipants(e.target.value)}
+                    onFocus={clearFields}
+                />
+            </StyledInputContainer>
+            <StyledGetButton
+                style={{marginRight: "auto", marginLeft: "auto", minHeight: "50px"}}
+                onClick={getParticipantsActivity}
+            >
+                Give me the activity!
+            </StyledGetButton>
+            {participantsActivity.activity ? (
+                <React.Fragment>
+                    <FavoriteButton activity={participantsActivity} setActivity={setParticipantsActivity}/>
+                    <ActivityCardDetails activity={participantsActivity}/>
+                </React.Fragment>
+            ) : (
+                <div style={{margin: "20px auto"}}>No activity was found!</div>
+            )}
+        </StyledActivityContainer>
+    ) : (
+        <StyledActivityContainer>
+            <StyledInputContainer>
+                Number of participants:
+                <input
+                    className="input"
+                    type="text"
+                    value={inputParticipants}
+                    placeholder={inputParticipants}
+                    onChange={(e) => setParticipants(e.target.value)}
+                    onFocus={clearFields}
+                />
+            </StyledInputContainer>
+            <StyledGetButton
+                style={{marginRight: "auto", marginLeft: "auto", minHeight: "50px"}}
+                onClick={getParticipantsActivity}
+            >
+                Give me the activity!
+            </StyledGetButton>
+        </StyledActivityContainer>
     );
 }
