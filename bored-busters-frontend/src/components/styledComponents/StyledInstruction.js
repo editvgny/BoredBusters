0f2e383966e04426a4142f009ca351d0c95491e4
editvgny <edit.vgny@gmail.com>
2021-06-10 11:28:51 +0200

    Formatted StyledInstruction.js, StyledSearchBox.js and StyledSearchCard.js

--- bored-busters-frontend/src/components/styledComponents/StyledInstruction.js
+++ bored-busters-frontend/src/components/styledComponents/StyledInstruction.js
@@ -1,12 +1,10 @@
-import  styled from 'styled-components';
+import styled from 'styled-components';
 
 export default styled.p`
-
       text-align: center;
       font-family: 'Mountains of Christmas';
       font-size: 30px;
       font-weight: 900;
       letter-spacing: 7px;
       color: #72CD55;
-
 `;
--- bored-busters-frontend/src/components/styledComponents/StyledSearchCard.js
+++ bored-busters-frontend/src/components/styledComponents/StyledSearchCard.js
@@ -1,11 +1,9 @@
-import  styled from 'styled-components';
+import styled from 'styled-components';
 
 export default styled.div`
- 
     margin: 20px;
     width: 820px;
     min-height: 100px;
     background: #172251;
     border-radius: 10px;
-
 `;
